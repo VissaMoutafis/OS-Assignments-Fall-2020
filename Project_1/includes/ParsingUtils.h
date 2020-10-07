0b3c573bbbe158b84318e904fcabdde1b138c795
VissaMoutafis <sdi1800119@di.uoa.gr>
2020-10-07 15:29:04 +0300

    added some comments to ease the caller

--- Project_1/includes/ParsingUtils.h
+++ Project_1/includes/ParsingUtils.h
@@ -11,5 +11,10 @@ char **parse_line(char *data_str, int *columns, char* sep);
 bool is_numeric(char* str);
 
 // arg parsing
+
+// input argc and argv from the termina
+// input string array pointer to place the input, and int* for the input size
+// allowed arguments THAT REQUIRE VALUE (i.e. -i "file") array for proper error checking (don't let the user check for basic errors i.e. null argument value)
 void args_parser(int argc, char **argv, char ***input, int *input_size, char** allowed_arguments);
-void parse_cnfg(char* filename); // parse config file
+// special function to parse config file
+void parse_cnfg(char* filename); 
